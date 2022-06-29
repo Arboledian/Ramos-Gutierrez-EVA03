bd635959fe9789551836c970b14bbfb6b387611e
Astray <104534314+Arboledian@users.noreply.github.com>
2022-06-28 22:48:58 -0300

    Add files via upload

--- Eva03.js
+++ Eva03.js
@@ -1,25 +1,22 @@
 function capturar() {
     console.log("capturado");
-    function Movie(name, genre, year) {
-        this.name = name;
-        this.genre = genre;
-        this.year = year;
+    function Meme(Software, Empresa, Usuario) {
+        this.Software = Software;
+        this.Empresa = Empresa;
+        this.Usuario = Usuario;
     }
-    var capturename = document.getElementById("name").value;
-    console.log(capturename);
-    var capturegenre = document.getElementById("genre").value;
-    console.log(capturegenre);
-    var captureyear = document.getElementById("year").value;
-    console.log(captureyear);
-    //var suejeto= new Persona("Kurt", 27); //captura estatica
-    // varialble local var sujeto= new Persona(capturarnombre, capturaredad);
-    sujeto = new Movie(capturename, capturegenre, captureyear);//variable global
-    //console.log(sujeto);
+    var captureSoftware = document.getElementById("Software").value;
+    console.log(captureSoftware);
+    var captureEmpresa = document.getElementById("Empresa").value;
+    console.log(captureEmpresa);
+    var captureUsuario = document.getElementById("Usuario").value;
+    console.log(captureUsuario);
+    sujeto = new Meme(captureSoftware, captureEmpresa, captureUsuario);
     agregar();
 }
 var basedatos = [];
 function agregar() {
     basedatos.push(sujeto)
     console.log(basedatos);
-    document.getElementById("tabla").innerHTML += '<tbody><td>' + sujeto.name + '</td><td>' + sujeto.genre + '</td><td>' + sujeto.year + '</td></tbody>';
+    document.getElementById("tabla").innerHTML += '<tbody><td>' + sujeto.Software + '</td><td>' + sujeto.Empresa  + '</td><td>' + sujeto.Usuario + '</td></tbody>';
 };
