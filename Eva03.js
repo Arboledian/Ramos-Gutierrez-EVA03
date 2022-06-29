51c51045ef25ed8c89fa487f1a65528868a580d1
Astray <104534314+Arboledian@users.noreply.github.com>
2022-06-29 17:24:53 -0400

    Actualización de los 3 archivos

--- Eva03.js
+++ Eva03.js
@@ -14,9 +14,62 @@ function capturar() {
     sujeto = new Meme(captureSoftware, captureEmpresa, captureUsuario);
     agregar();
 }
+function gbtn() {
+
+    button.style.background = "grey";
+    button2.style.background = "orange";
+    button3.style.background = "gold";
+    div.style.background = "red";
+    div2.style.background = "red";
+}
+function gbtn2() {
+    button.style.background = "red";
+    button2.style.background = "grey";
+    button3.style.background = "gold";
+    div.style.background = "orange";
+    div2.style.background = "orange";
+}
+function gbtn3() {
+    button.style.background = "red";
+    button2.style.background = "orange";
+    button3.style.background = "grey";
+    div.style.background = "gold";
+    div2.style.background = "gold";
+}
+
 var basedatos = [];
 function agregar() {
     basedatos.push(sujeto)
     console.log(basedatos);
     document.getElementById("tabla").innerHTML += '<tbody><td>' + sujeto.Software + '</td><td>' + sujeto.Empresa  + '</td><td>' + sujeto.Usuario + '</td></tbody>';
 };
+        var h1 = document.getElementById("Software");
+        console.log(h1.innerHTML);
+        h1.style.color = "blue";
+        var h2 = document.getElementById("Empresa");
+        console.log(h2.innerHTML);
+        h2.style.color = "gold";
+        var h3 = document.getElementById("Usuario");
+        console.log(h2.innerHTML);
+        h3.style.color = "red";
+var button = document.getElementById("boton");
+console.log(button.innerHTML);
+button.style.color = "white";
+button.style.background = "red";
+
+var button2 = document.getElementById("boton2");
+console.log(button2.innerHTML);
+button2.style.color = "white";
+button2.style.background = "orange";
+
+
+var button3 = document.getElementById("boton3");
+console.log(button2.innerHTML);
+button3.style.color = "white";
+button3.style.background = "gold";
+
+
+var div = document.getElementById("titulo");
+console.log(div.innerHTML);
+var div2 = document.getElementById("user");
+console.log(div2.innerHTML);
